499bf02d2dd7240340585a9e2ea2b28e2a5ca332
Vadino Guerrero <vadino.guerrero@example.com>
2026-06-27 21:45:50 -0300

    Corrected header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header style={{ padding: '1rem', backgroundColor: '#282c34', color: '#fff' }}>
+    <header style={{ padding: '1rem ', backgroundColor: '#282c34', color: '#fff',  boxShadow: '0px 50px #000', zIndex:'10' }}>
       <div style={{ maxWidth: '960px', margin: '0 auto', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
         <h1 style={{ margin: 0, fontSize: '1.5rem' }}>Personal Blog</h1>
         <nav>
